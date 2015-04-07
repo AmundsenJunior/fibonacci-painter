ca406c6052937c316a3e47c99af5157b0c821a60
Scott Russell <scottedwardrussell@gmail.com>
2015-04-07 13:54:45 -0400

    Rewrote fibonacci-static in p5js and updated index to point to fibonacci-static

---
+++ processing/fibonacci-static.js
@@ -0,0 +1,24 @@
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    var x_pos = mouseX;
+    var y_pos = mouseY;
+    fibonacci(x_pos, y_pos);
+  }
+}    
+  
+function fibonacci(x, y) {
+  stroke(255);
+  noFill();
+  arc(x, y, 10, 10, PI, TWO_PI);
+  arc(x-5, y, 20, 20, 0, HALF_PI);
+  arc(x-5, y-5, 30, 30, HALF_PI, PI);
+  arc(x+5, y-5, 50, 50, PI, PI+HALF_PI);
+  arc(x+5, y+10, 80, 80, PI+HALF_PI, TWO_PI);
+  arc(x-20, y+10, 130, 130, 0, HALF_PI);
+}
