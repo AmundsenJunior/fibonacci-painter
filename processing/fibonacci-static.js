96ade02182d17c20e680c62a759da1f6edeac4cb
Scott Russell <scottedwardrussell@gmail.com>
2015-04-07 17:15:07 -0400

    Working on index and fibonacci-static in instance mode -- Not Yet Working

--- processing/fibonacci-static.js
+++ processing/fibonacci-static.js
@@ -1,25 +1,29 @@
+var fibStatSketch = function( fibStat ) {
 
-function setup() {
-  var canvas = createCanvas(500, 500);
-  canvas.parent('fibonacci-static');
-  background(0);
-}
+  fibStat.setup = function() {
+    fibStat.canvas = createCanvas(500, 500);
+    fibStat.background(0);
+  };
 
-function draw() {
-  if (mouseIsPressed) {
-    var x_pos = mouseX;
-    var y_pos = mouseY;
-    fibonacci(x_pos, y_pos);
-  }
-}    
+  fibStat.draw = function() {
+    if (mouseIsPressed) {
+      var x_pos = mouseX;
+      var y_pos = mouseY;
+      fibStat.fibonacci(x_pos, y_pos);
+    }
+  };    
   
-function fibonacci(x, y) {
-  stroke(255);
-  noFill();
-  arc(x, y, 10, 10, PI, TWO_PI);
-  arc(x-5, y, 20, 20, 0, HALF_PI);
-  arc(x-5, y-5, 30, 30, HALF_PI, PI);
-  arc(x+5, y-5, 50, 50, PI, PI+HALF_PI);
-  arc(x+5, y+10, 80, 80, PI+HALF_PI, TWO_PI);
-  arc(x-20, y+10, 130, 130, 0, HALF_PI);
-}
+  fibStat.fibonacci = function(x, y) {
+    fibStat.stroke(255);
+    fibStat.noFill();
+    fibStat.arc(x, y, 10, 10, PI, TWO_PI);
+    fibStat.arc(x-5, y, 20, 20, 0, HALF_PI);
+    fibStat.arc(x-5, y-5, 30, 30, HALF_PI, PI);
+    fibStat.arc(x+5, y-5, 50, 50, PI, PI+HALF_PI);
+    fibStat.arc(x+5, y+10, 80, 80, PI+HALF_PI, TWO_PI);
+    fibStat.arc(x-20, y+10, 130, 130, 0, HALF_PI);
+  };
+};
+
+var fibonacciStatic = new p5(fibStatSketch, 'fibonacci-static');
+
